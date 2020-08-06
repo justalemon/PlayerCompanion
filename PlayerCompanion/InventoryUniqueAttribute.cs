e528db98a0b93af9905135723ad3fc1b18930001
Hannele Ruiz <justlemoncl@gmail.com>
2020-08-06 07:52:28 -0400

    Allow some types to be marked as Unique

---
+++ PlayerCompanion/InventoryUniqueAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PlayerCompanion
+{
+    /// <summary>
+    /// Marks a Class as a Unique Inventory Item.
+    /// Unique Inventory Items can't be found in the wild and will not be returned when calling <see cref="Inventory.CreateRandomItem()"/>
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class InventoryUniqueAttribute : Attribute
+    {
+    }
+}
