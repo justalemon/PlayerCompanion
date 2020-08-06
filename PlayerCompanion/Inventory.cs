8c5dada5dbd3a924cef4455a5c0da1588381d726
Hannele Ruiz <justlemoncl@gmail.com>
2020-08-06 07:50:31 -0400

    Added working inventory system

--- PlayerCompanion/Inventory.cs
+++ PlayerCompanion/Inventory.cs
@@ -1,6 +1,8 @@
 ﻿using GTA;
+using GTA.UI;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 
@@ -15,7 +17,10 @@ namespace PlayerCompanion
 
         private static readonly string locationMod = new Uri(Path.GetDirectoryName(Assembly.GetCallingAssembly().CodeBase)).LocalPath;
         private static readonly string locationFolder = Path.Combine(locationMod, "PlayerCompanion");
+        private static readonly string locationInventoryData = Path.Combine(locationFolder, "Inventory");
         private static readonly string locationConfig = Path.Combine(locationFolder, "Inventory.json");
+
+        private static readonly Dictionary<Model, PedInventory> inventories = new Dictionary<Model, PedInventory>();
 
         #endregion
 
@@ -29,6 +34,25 @@ namespace PlayerCompanion
         /// The configuration of the inventory.
         /// </summary>
         public static InventoryConfiguration Configuration { get; private set; } = null;
+        /// <summary>
+        /// The inventory of the current Ped Model.
+        /// </summary>
+        public static PedInventory Current
+        {
+            get
+            {
+                // If the inventory is shared across all peds, return the generic inventory
+                if (Configuration.SharedInventory)
+                {
+                    return GetInventory(0);
+                }
+                // Otherwise, return the inventory for the specific ped model
+                else
+                {
+                    return GetInventory(Game.Player.Character.Model);
+                }
+            }
+        }
 
         #endregion
 
@@ -72,6 +96,50 @@ namespace PlayerCompanion
             Directory.CreateDirectory(locationFolder);
             // And dumps the contents of the file
             File.WriteAllText(locationConfig, contents);
+        }
+        /// <summary>
+        /// Gets the Inventory of a specific <see cref="Ped"/> <see cref="Model"/>.
+        /// </summary>
+        /// <param name="model">The Model of the <see cref="Ped"/>.</param>
+        /// <returns>The <see cref="PedInventory"/> that contains the items of the <see cref="Ped"/> <see cref="Model"/>.</returns>
+        public static PedInventory GetInventory(Model model)
+        {
+            // If the model is not a ped, raise an exception
+            if (!model.IsPed)
+            {
+                throw new InvalidOperationException("Model is not a Ped or is not present in the game files.");
+            }
+
+            // If there is an inventory in the dictionary, return it
+            if (inventories.ContainsKey(model))
+            {
+                return inventories[model];
+            }
+
+            // Make the location of the file for the ped model
+            string file = Path.Combine(locationInventoryData, $"{model.Hash}.json");
+
+            // If there is none, create a new one and send it
+            if (!File.Exists(file))
+            {
+                PedInventory empty = new PedInventory(model);
+                inventories[model] = empty;
+                return empty;
+            }
+
+            // Load the contents of the file
+            string contents = File.ReadAllText(file);
+            // Try to parse it
+            List<Item> items = JsonConvert.DeserializeObject<List<Item>>(contents);
+            // If any of them are null, notify the user
+            if (items.RemoveAll(item => item == null) != 0)
+            {
+                Notification.Show("~r~Danger~s~: One of the items could not be parsed. This might represent a missing mod.");
+            }
+            // Finally, create a new inventory and save it
+            PedInventory inventory = new PedInventory(model, items.ToArray());
+            inventories[model] = inventory;
+            return inventory;
         }
 
         #endregion
---
+++ PlayerCompanion/PedInventory.cs
@@ -0,0 +1,83 @@
+﻿using GTA;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace PlayerCompanion
+{
+    /// <summary>
+    /// Represents an Inventory for a specific <see cref="Ped"/> <see cref="GTA.Model"/>.
+    /// </summary>
+    public class PedInventory
+    {
+        #region Private Fields
+
+        private static readonly string locationMod = new Uri(Path.GetDirectoryName(Assembly.GetCallingAssembly().CodeBase)).LocalPath;
+        private static readonly string locationInventoryData = Path.Combine(locationMod, "PlayerCompanion", "Inventory");
+
+        private readonly List<Item> items = new List<Item>();
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// The <see cref="Ped"/> <see cref="GTA.Model"/> assigned to this inventory.
+        /// </summary>
+        public Model Model { get; }
+
+        #endregion
+
+        #region Constructors
+
+        internal PedInventory(Model model, params Item[] items)
+        {
+            Model = model;
+            this.items.AddRange(items);
+        }
+
+        #endregion
+
+        #region Public Functions
+
+        /// <summary>
+        /// Adds a specific item onto this inventory.
+        /// </summary>
+        /// <param name="item">The item added.</param>
+        public void Add(Item item)
+        {
+            // If the item is already on the list, return
+            if (items.Contains(item))
+            {
+                throw new InvalidOperationException("This item is already part of this inventory.");
+            }
+
+            // Try to find an item with the same type
+            foreach (Item existingItem in items)
+            {
+                // If the types match, raise an exception
+                if (existingItem.GetType() == item.GetType())
+                {
+                    throw new InvalidOperationException("This inventory already has an item with the same type.");
+                }
+            }
+
+            // Otherwise, add it and save the item
+            items.Add(item);
+            Save();
+        }
+        /// <summary>
+        /// Saves this <see cref="PedInventory"/>.
+        /// </summary>
+        public void Save()
+        {
+            Directory.CreateDirectory(locationInventoryData);
+            string json = JsonConvert.SerializeObject(items);
+            File.WriteAllText(Path.Combine(locationInventoryData, $"{Model.Hash}.json"), json);
+        }
+
+        #endregion
+    }
+}
