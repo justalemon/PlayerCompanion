589581befbd8099acb5b9a9a70b47a1fbe7a993b
Hannele Ruiz <justlemoncl@gmail.com>
2020-08-06 07:36:19 -0400

    Added basic Item with JSON Converter

---
+++ PlayerCompanion/Item.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+
+namespace PlayerCompanion
+{
+    /// <summary>
+    /// Represents an item that can be added to the inventory.
+    /// </summary>}
+    [JsonConverter(typeof(ItemConverter))]
+    public abstract class Item
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The total number of items.
+        /// </summary>
+        public virtual int Count { get; set; }
+        /// <summary>
+        /// The maximum number of this single item.
+        /// </summary>
+        public abstract int Max { get; }
+        /// <summary>
+        /// The name of the item.
+        /// </summary>
+        public abstract string Name { get; }
+        /// <summary>
+        /// The Dictionary where the Item image could be found.
+        /// </summary>
+        public virtual string ImageDictionary => "";
+        /// <summary>
+        /// The Sprite of the Item image.
+        /// </summary>
+        public virtual string ImageSprite => "";
+        /// <summary>
+        /// If this inventory item has a valid image to use.
+        /// </summary>
+        public virtual bool HasImage => false;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates a new <see cref="Item"/> with a specific count.
+        /// </summary>
+        /// <param name="count">The total number of this <see cref="Item"/>.</param>
+        public Item(int count)
+        {
+            Count = count;
+        }
+
+        #endregion
+    }
+}
---
+++ PlayerCompanion/ItemConverter.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace PlayerCompanion
+{
+    /// <summary>
+    /// Serializes and Deserializes inventory items.
+    /// </summary>
+    public class ItemConverter : JsonConverter<Item>
+    {
+        /// <summary>
+        /// Reads an <see cref="Item"/>.
+        /// </summary>
+        /// <returns></returns>
+        public override Item ReadJson(JsonReader reader, Type objectType, Item existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            // Load the entire thing into a JSON Object
+            JObject @object = JObject.Load(reader);
+            // Try to get the type of the object
+            Type type = Type.GetType((string)@object["type"], false);
+            // If no item was found, return null
+            if (type == null)
+            {
+                return null;
+            }
+            // Otherwise, create a new instance and return a generic inventory item
+            return (Item)Activator.CreateInstance(type, (int)@object["count"]);
+        }
+        /// <summary>
+        /// Writes an <see cref="Item"/>.
+        /// </summary>
+        public override void WriteJson(JsonWriter writer, Item value, JsonSerializer serializer)
+        {
+            // Create a new JObject with the values
+            JObject @object = new JObject
+            {
+                ["type"] = value.GetType().ToString(),
+                ["count"] = value.Count
+            };
+            // And save it
+            @object.WriteTo(writer);
+        }
+    }
+}
