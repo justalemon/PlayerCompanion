6c1ef6a9d41c8a63b69e7a97cf6fee4973edf9e8
Hannele Ruiz <justlemoncl@gmail.com>
2020-08-10 18:45:08 -0400

    Color Objects now default to specific values if they are missing

--- PlayerCompanion/Converters/ColorConverter.cs
+++ PlayerCompanion/Converters/ColorConverter.cs
@@ -16,14 +16,22 @@ namespace PlayerCompanion.Converters
         /// <returns>The <see cref="Color"/> created from the JSON Object.</returns>
         public override Color ReadJson(JsonReader reader, Type objectType, Color existingValue, bool hasExistingValue, JsonSerializer serializer)
         {
+            // Load the contents of the reader
             JObject @object = JObject.Load(reader);
-            return Color.FromArgb((int)@object["a"], (int)@object["r"], (int)@object["g"], (int)@object["b"]);
+            // Get the RGBA values (default to Black if the component is not present)
+            int red = @object.ContainsKey("r") ? (int)@object["r"] : 0;
+            int green = @object.ContainsKey("g") ? (int)@object["g"] : 0;
+            int blue = @object.ContainsKey("b") ? (int)@object["b"] : 0;
+            int alpha = @object.ContainsKey("a") ? (int)@object["a"] : 255;
+            // And return a Color struct
+            return Color.FromArgb(alpha, red, green, blue);
         }
         /// <summary>
         /// Writes the <see cref="Color"/> as a JSON Object.
         /// </summary>
         public override void WriteJson(JsonWriter writer, Color value, JsonSerializer serializer)
         {
+            // Create a JSON Object with the RGBA Values
             JObject @object = new JObject
             {
                 ["r"] = value.R,
@@ -31,6 +39,7 @@ namespace PlayerCompanion.Converters
                 ["b"] = value.B,
                 ["a"] = value.A
             };
+            // And write it to the file.
             @object.WriteTo(writer);
         }
     }
