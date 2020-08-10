8b7dcd8bc8859c19d0861f4fa76360b146d643f7
Hannele Ruiz <justlemoncl@gmail.com>
2020-08-10 19:57:32 -0400

    Create a special item when is not possible to load it

--- PlayerCompanion/Converters/ItemConverter.cs
+++ PlayerCompanion/Converters/ItemConverter.cs
@@ -1,6 +1,8 @@
-﻿using Newtonsoft.Json;
+﻿using GTA.UI;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 
 namespace PlayerCompanion.Converters
 {
@@ -17,12 +19,23 @@ namespace PlayerCompanion.Converters
         {
             // Load the entire thing into a JSON Object
             JObject @object = JObject.Load(reader);
+
+            // If there is no Type in the object, raise an exception
+            if (!@object.ContainsKey("type"))
+            {
+                throw new KeyNotFoundException("There was no Type specified in the class.");
+            }
+
+            // Get the count of this item
+            int count = @object.ContainsKey("count") ? (int)@object["count"] : 0;
+
             // Try to get the type of the object
             Type type = Type.GetType((string)@object["type"], false);
-            // If no item was found, return null
+            // If the type was not found, return an invalid type
             if (type == null)
             {
-                return null;
+                Notification.Show($"~r~Danger~s~: {(string)@object["type"]} was not found. Make sure that all of the required mods are installed and try again.");
+                return new InvalidItem((string)@object["type"], count);
             }
             // Otherwise, create a new instance and return a generic inventory item
             return (Item)Activator.CreateInstance(type, (int)@object["count"]);
@@ -32,12 +45,20 @@ namespace PlayerCompanion.Converters
         /// </summary>
         public override void WriteJson(JsonWriter writer, Item value, JsonSerializer serializer)
         {
-            // Create a new JObject with the values
-            JObject @object = new JObject
+            // Create a new JObject to store the values
+            JObject @object = new JObject();
+            // If this is an invalid item, save the type in the property
+            if (value is InvalidItem invalid)
             {
-                ["type"] = value.GetType().ToString(),
-                ["count"] = value.Count
-            };
+                @object["type"] = invalid.Type;
+            }
+            // Otherwise, save the real Type
+            else
+            {
+                @object["type"] = value.GetType().ToString();
+            }
+            // Store the total count of this item
+            @object["count"] = value.Count;
             // And save it
             @object.WriteTo(writer);
         }
---
+++ PlayerCompanion/InvalidItem.cs
@@ -0,0 +1,36 @@
+﻿namespace PlayerCompanion
+{
+    /// <summary>
+    /// Represents an Item that can't be used.
+    /// Invalid Items are automatically created when a menu item can't be restored.
+    /// </summary>
+    public sealed class InvalidItem : Item
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The Name of this Invalid Item.
+        /// </summary>
+        public override string Name { get; }
+        /// <summary>
+        /// The Price of this Invalid Item.
+        /// </summary>
+        public override int Price { get; } = 0;
+        /// <summary>
+        /// The Type that could not be created during initialization.
+        /// </summary>
+        public string Type { get; }
+
+        #endregion
+
+        #region Constructors
+
+        internal InvalidItem(string type, int count) : base(count)
+        {
+            Name = $"Invalid ({type})";
+            Type = type;
+        }
+
+        #endregion
+    }
+}
