783b7dfa7abb0d658035aeec0ac421fedc94d11c
Hannele Ruiz <justlemoncl@gmail.com>
2020-11-05 18:25:48 -0300

    Improved the InventoryUniqueAttribute

--- PlayerCompanion/Inventory/Attributes.cs
+++ PlayerCompanion/Inventory/Attributes.cs
@@ -4,11 +4,32 @@ namespace PlayerCompanion
 {
     /// <summary>
     /// Marks a Class as a Unique Inventory Item.
-    /// Unique Inventory Items can't be found in the wild and will not be returned when calling <see cref="Inventory.CreateRandomItem()"/>
+    /// Unique Inventory Items will be ignored by <see cref="PedInventory.CreateRandomItem()"/>.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class)]
     public class InventoryUniqueAttribute : Attribute
     {
+        /// <summary>
+        /// If the Inventory Item is unique or not.
+        /// </summary>
+        public bool Unique { get; private set; } = false;
+
+        /// <summary>
+        /// Marks an item as unique.
+        /// </summary>
+        public InventoryUniqueAttribute()
+        {
+            Unique = true;
+        }
+
+        /// <summary>
+        /// Marks an item as unique or not.
+        /// </summary>
+        /// <param name="unique"><see langword="true"/> if the item should be Unique, <see langword="false"/> otherwise.</param>
+        public InventoryUniqueAttribute(bool unique)
+        {
+            Unique = unique;
+        }
     }
 
     /// <summary>
