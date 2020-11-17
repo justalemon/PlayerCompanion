49dc9b79480ce72507bca3d652976e34f08ed4dd
Hannele Ruiz <justlemoncl@gmail.com>
2020-11-17 07:59:50 -0300

    Added events and functions for Adding and Removing items

---
+++ PlayerCompanion/InventoryEvents.cs
@@ -0,0 +1,37 @@
+﻿namespace PlayerCompanion
+{
+    /// <summary>
+    /// Represents the event triggered when an Inventory Item is changed.
+    /// </summary>
+    /// <param name="sender">The Inventory that triggered the event.</param>
+    /// <param name="e">The Item information.</param>
+    public delegate void ItemChangedEventHandler(object sender, ItemChangedEventArgs e);
+
+    /// <summary>
+    /// The Item information of an item when is changed.
+    /// </summary>
+    public class ItemChangedEventArgs
+    {
+        #region Properties
+
+        /// <summary>
+        /// The item that was changed.
+        /// </summary>
+        public Item Item { get; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates a new <see cref="ItemChangedEventArgs"/>.
+        /// </summary>
+        /// <param name="item">The item that was Changed.</param>
+        public ItemChangedEventArgs(Item item)
+        {
+            Item = item;
+        }
+
+        #endregion
+    }
+}
--- PlayerCompanion/InventoryManager.cs
+++ PlayerCompanion/InventoryManager.cs
@@ -28,6 +28,19 @@ namespace PlayerCompanion
 
         #endregion
 
+        #region Events
+
+        /// <summary>
+        /// Event triggered when an Item is Added to this Inventory.
+        /// </summary>
+        public event ItemChangedEventHandler ItemAdded;
+        /// <summary>
+        /// Event triggered when an Item is Removed from this Inventory.
+        /// </summary>
+        public event ItemChangedEventHandler ItemRemoved;
+
+        #endregion
+
         #region Constructor
 
         internal PedInventory(Model model, InventoryManager manager, List<Item> items)
@@ -39,6 +52,51 @@ namespace PlayerCompanion
                 this.items.AddRange(items);
             }
         }
+
+        #endregion
+
+        #region Functions
+
+        /// <summary>
+        /// Adds an item to this inventory.
+        /// </summary>
+        /// <param name="item">The item to add.</param>
+        public void Add(Item item)
+        {
+            // If the item is already present, raise an exception
+            if (items.Contains(item))
+            {
+                throw new InvalidOperationException("The Item is already part of the inventory.");
+            }
+            // Otherwise, add it and trigger the events
+            items.Add(item);
+            ItemChangedEventArgs e = new ItemChangedEventArgs(item);
+            ItemAdded?.Invoke(this, e);
+            manager.OnItemAdded(this, e);
+        }
+        /// <summary>
+        /// Removes an item from the inventory.
+        /// </summary>
+        /// <param name="item">The item to remove.</param>
+        public void Remove(Item item)
+        {
+            // If the item is not part of the menu, just return
+            if (items.Contains(item))
+            {
+                return;
+            }
+            // Otherwise, remove it and trigger the events
+            items.Remove(item);
+            ItemChangedEventArgs e = new ItemChangedEventArgs(item);
+            ItemRemoved?.Invoke(this, e);
+            manager.OnItemRemoved(this, e);
+        }
+        /// <summary>
+        /// Checks if the item is part of this Inventory.
+        /// </summary>
+        /// <param name="item">The item to check.</param>
+        /// <returns><see langword="true"/> if the item is on the inventory, <see langword="false"/> otherwise.</returns>
+        public bool Contains(Item item) => items.Contains(item);
 
         #endregion
     }
@@ -89,6 +147,21 @@ namespace PlayerCompanion
                 return @new;
             }
         }
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Event triggered when an Item is Added to any Inventory.
+        /// </summary>
+        public event ItemChangedEventHandler ItemAdded;
+        internal void OnItemAdded(PedInventory inventory, ItemChangedEventArgs e) => ItemAdded?.Invoke(inventory, e);
+        /// <summary>
+        /// Event triggered when an Item is Removed from any Inventory.
+        /// </summary>
+        public event ItemChangedEventHandler ItemRemoved;
+        internal void OnItemRemoved(PedInventory inventory, ItemChangedEventArgs e) => ItemRemoved?.Invoke(inventory, e);
 
         #endregion
 
