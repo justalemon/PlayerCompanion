62b0527a1257ae61d78946a93459567d500e97cf
Hannele Ruiz <justlemoncl@gmail.com>
2021-07-27 19:45:05 -0400

    Moved color related stuff to it's own folder

--- PlayerCompanion/Color/ColorManager.cs
+++ PlayerCompanion/Color/ColorManager.cs
@@ -11,40 +11,6 @@ using System.IO;
 namespace PlayerCompanion
 {
     /// <summary>
-    /// Serializes and Deserializes <see cref="Color"/>.
-    /// </summary>
-    internal class ColorConverter : JsonConverter<Color>
-    {
-        /// <summary>
-        /// Reads a JSON Object as a <see cref="Color"/>.
-        /// </summary>
-        /// <returns>The <see cref="Color"/> created from the JSON Object.</returns>
-        public override Color ReadJson(JsonReader reader, Type objectType, Color existingValue, bool hasExistingValue, JsonSerializer serializer)
-        {
-            JObject @object = JObject.Load(reader);
-            int red = @object.ContainsKey("r") ? (int)@object["r"] : 0;
-            int green = @object.ContainsKey("g") ? (int)@object["g"] : 0;
-            int blue = @object.ContainsKey("b") ? (int)@object["b"] : 0;
-            int alpha = @object.ContainsKey("a") ? (int)@object["a"] : 255;
-            return Color.FromArgb(alpha, red, green, blue);
-        }
-        /// <summary>
-        /// Writes the <see cref="Color"/> as a JSON Object.
-        /// </summary>
-        public override void WriteJson(JsonWriter writer, Color value, JsonSerializer serializer)
-        {
-            JObject @object = new JObject
-            {
-                ["r"] = value.R,
-                ["g"] = value.G,
-                ["b"] = value.B,
-                ["a"] = value.A
-            };
-            @object.WriteTo(writer);
-        }
-    }
-
-    /// <summary>
     /// Manages the Colors on the HUD and Radar/Map for the Players.
     /// </summary>
     public class ColorManager
