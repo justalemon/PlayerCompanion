404f8aa2467195646ff2310150d352d25f588990
Hannele Ruiz <justlemoncl@gmail.com>
2022-06-23 07:23:06 -0400

    Added description of the item in the inventory

--- PlayerCompanion/Inventory/InventoryItem.cs
+++ PlayerCompanion/Inventory/InventoryItem.cs
@@ -18,7 +18,7 @@ namespace PlayerCompanion
 
         #region Constructor
 
-        internal InventoryItem(Item item) : base(item.Name, "", item is StackableItem s ? s.Count.ToString() : "")
+        internal InventoryItem(Item item) : base(item.Name, item.Description, item is StackableItem s ? s.Count.ToString() : "")
         {
             Item = item;
             Activated += (sender, e) => Item.Use();
